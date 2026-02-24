3014263dcc2e899d00d2215795a99d02289995bc
808Code <dikshant.shah2k@gmail.com>
2026-02-24 20:07:58 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { supabase } from "./supabaseClient";
 import AdminLogin from "./AdminLogin";
 import UpdatePage from "./UpdatePage";
